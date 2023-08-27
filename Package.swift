8cae912609eb06fb3aad9c2797fa7b145f807f73
Buns <bunsthedev@gmail.com>
2023-08-27 00:56:45 -0500

    fix: Package

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
             .package(url: "https://github.com/SnapKit/SnapKit.git", .upToNextMajor(from: "5.0.1")),
             .package(url: "https://github.com/horizontalsystems/HUD.Swift.git", .upToNextMajor(from: "2.0.0")),
             .package(url: "https://github.com/horizontalsystems/SectionsTableView.Swift.git", .upToNextMajor(from: "1.0.0")),
-            .package(url: "https://github.com/horizontalsystems/SoulSwapThemeKit.git", .upToNextMajor(from: "1.0.0")),
+            .package(url: "https://github.com/SoulSwapFinance/SoulSwapThemeKit.git", .upToNextMajor(from: "1.0.0")),
             .package(url: "https://github.com/horizontalsystems/UIExtensions.Swift.git", .upToNextMajor(from: "1.0.0")),
         ],
         targets: [
